67a1d93d9a866bbf36f996cb79dae0cafdd79398
Hanty <hpdh0731@hotmail.com>
2014-09-25 20:45:57 +0800

    finish fibonacci assembly code

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -14,19 +14,32 @@ fibonacci:
 	push {r3, r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	subs R4, R0, #0
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	@ if(R0 <= 0) goto .L3 (which returns 0)
+	ble .L3
+
+	@ If(R4 == 1) goto .L4 (which returns 1)
+	cmp R4, #1
+	beq .L4
 
 	@ R0 = R4 - 1
+	sub R0, R4, #1
+
 	@ Recursive call to fibonacci with R4 - 1 as parameter
+	bl fibonacci
 
 	@ R5 = R0
+	mov R5, R0
+
 	@ R0 = R4 - 2
+	subs R0, R4, #2
+
 	@ Recursive call to fibonacci with R4 - 2 as parameter
+	bl fibonacci
 
 	@ R0 = R5 + R0 (update flags)
+	adds R0, R5, R0
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
